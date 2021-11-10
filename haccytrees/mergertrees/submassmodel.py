346145cf5fa645cec7cf747057820b7542a0a164
Michael Buehlmann <buehlmann.michi@gmail.com>
2021-11-10 12:51:21 -0600

    fix submassmodel to use current host mass instead of host mass at infall

--- haccytrees/mergertrees/submassmodel.py
+++ haccytrees/mergertrees/submassmodel.py
@@ -5,12 +5,15 @@ from ..simulations import Simulation, Cosmology
 
 # TODO: parallelize numba functions: act on each root individually
 
+
 @numba.jit(nopython=True)
-def _create_submass_hostidx(snapnum, desc_index, snap0, submass_hostidx, submass_infallidx, submass_snapnum):
+def _create_submass_hostidx(
+    snapnum, desc_index, snap0, submass_hostidx, submass_infallidx, submass_snapnum
+):
     nsub = len(submass_hostidx)
     nhalos = len(snapnum)
     # A temporary array to keep track of the hosts in the current (sub)tree
-    snap_roots = np.empty(snap0+1, dtype=np.int64)
+    snap_roots = np.empty(snap0 + 1, dtype=np.int64)
     snap_roots[:] = -1
     lastsnap = snapnum[0]
     current_idx = 0
@@ -32,36 +35,54 @@ def _create_submass_hostidx(snapnum, desc_index, snap0, submass_hostidx, submass
         snap_roots[snapnum[i]] = i
         lastsnap = snapnum[i]
 
-    assert(current_idx == nsub)
+    assert current_idx == nsub
 
 
+# TODO: subhalo of subhalo should use parent subhalo's mass as host mass, not the main's
 @numba.jit(nopython=True)
-def _create_submass_model(snapnum, mass, desc_index, snap0, t_lb, tau_sub, zeta, submass):
+def _create_submass_model(
+    snapnum, mass, desc_index, snap0, t_lb, tau_sub, zeta, submass
+):
     nsub = len(submass)
     nhalos = len(mass)
 
-    current_idx = nsub-1
-    for i in range(nhalos-1, -1, -1):
-        if desc_index[i] != -1 and desc_index[i] != i-1:
+    current_idx = nsub - 1
+    for i in range(nhalos - 1, -1, -1):
+        if desc_index[i] != -1 and desc_index[i] != i - 1:
             # This halo merges
-            s = snapnum[i]
+            s_infall = snapnum[i]  # last snapshot the halo exists
             # the last halo merged between lastsnap and lastsnap+1
-            tlb_infall = 0.5*(t_lb[s] + t_lb[s+1])
+            t_lb_current = 0.5 * (t_lb[s_infall] + t_lb[s_infall + 1])
             # the mass of the host at the first timestep of the subhalo
             mass_host = mass[desc_index[i]]
-            # the infall mass
-            mass_infall = mass[i]
-            # some factors in the mass model equation
-            mass_fac = zeta * (mass_infall/mass_host)**zeta
-            exp_fac = -1/zeta
+            # start of the subhalo mass: the infall mass
+            mass_sub = mass[i]
+            # the exponential factor
+            exp_fac = -1 / zeta
             # loop over the snaps that need to be filled in
-            for j in range(s+1, snap0+1):
-                delta_t = tlb_infall - t_lb[j]
-                tau = tau_sub[j]  # from the paper: tau is computed at t+delta_t
-                submass[current_idx] = mass_infall * (1 + mass_fac*delta_t/tau)**exp_fac
+            for j in range(snap0 - s_infall):
+                # make sure we're writing to a valid position
+                assert current_idx >= 0
+                # current snapshot we're working on
+                s = s_infall + j + 1
+                # host mass of previous step
+                mass_host = mass[desc_index[i] - j]
+                mass_fac = zeta * (mass_sub / mass_host) ** zeta
+
+                delta_t = t_lb_current - t_lb[s]
+                tau = tau_sub[s]  # from the paper: tau is computed at t+delta_t
+                submass[current_idx] = (
+                    mass_sub * (1 + mass_fac * delta_t / tau) ** exp_fac
+                )
+
+                # update time and mass
+                t_lb_current = t_lb[s]
+                mass_sub = submass[current_idx]
+
+                # update index
                 current_idx -= 1
-                # assert(current_idx >= 0)
-    assert(current_idx == -1)
+
+    assert current_idx == -1
 
 
 @numba.jit(nopython=True)
@@ -74,7 +95,7 @@ def _create_submass_data(mass, snapnum, snap0, desc_index, tau_sub, t_lb, zeta):
         if snapnum[i] >= lastsnap and desc_index[i] >= 0:
             # we are at a new "row" in matrix-layout
             # everything from z=0 to that halo will be a sub
-            nsub += snap0-snapnum[i]
+            nsub += snap0 - snapnum[i]
         lastsnap = snapnum[i]
 
     # Allocate arrays
@@ -84,16 +105,22 @@ def _create_submass_data(mass, snapnum, snap0, desc_index, tau_sub, t_lb, zeta):
     submass_snapnum = np.zeros(nsub, dtype=snapnum.dtype)
 
     # Fill hostidx
-    _create_submass_hostidx(snapnum, desc_index, snap0, submass_hostidx, submass_infallidx, submass_snapnum)
-    
+    _create_submass_hostidx(
+        snapnum, desc_index, snap0, submass_hostidx, submass_infallidx, submass_snapnum
+    )
+
     # Fill submass (in reverse)
-    _create_submass_model(snapnum, mass, desc_index, snap0, t_lb, tau_sub, zeta, submass)
+    _create_submass_model(
+        snapnum, mass, desc_index, snap0, t_lb, tau_sub, zeta, submass
+    )
 
     return submass, submass_hostidx, submass_infallidx, submass_snapnum
 
 
 @numba.jit(nopython=True, parallel=True)
-def _compute_fsub_stats(subdata_offset, subdata_size, tree_node_mass, sub_mass, fsubtot, fsubmax):
+def _compute_fsub_stats(
+    subdata_offset, subdata_size, tree_node_mass, sub_mass, fsubtot, fsubmax
+):
     nhalos = len(tree_node_mass)
     for i in numba.prange(nhalos):
         start = subdata_offset[i]
@@ -110,7 +137,7 @@ def _compute_fsub_stats(subdata_offset, subdata_size, tree_node_mass, sub_mass, 
 @numba.jit(nopython=True)
 def _subdata_hostidx_order(nhalos, submass_hostidx):
     nsub = len(submass_hostidx)
-    
+
     # Count subhalos of each halo
     subdata_size = np.zeros(nhalos, dtype=np.int64)
     for i in range(nsub):
@@ -121,7 +148,7 @@ def _subdata_hostidx_order(nhalos, submass_hostidx):
     for i in range(nhalos):
         subdata_offset[i] = current_offset
         current_offset += subdata_size[i]
-    assert(current_offset == nsub)
+    assert current_offset == nsub
 
     # get ordering of data
     subdata_localoffsets = np.zeros(nhalos, dtype=np.int64)
@@ -132,7 +159,7 @@ def _subdata_hostidx_order(nhalos, submass_hostidx):
         subdata_s[i] = subdata_offset[hidx] + subdata_localoffsets[hidx]
         subdata_localoffsets[hidx] += 1
 
-    return subdata_s, subdata_offset, subdata_size    
+    return subdata_s, subdata_offset, subdata_size
 
 
 def create_submass_data(
@@ -143,7 +170,7 @@ def create_submass_data(
     A: float = 1.1,
     mass_threshold: float = None,
     compute_fsub_stats: bool = False
-    ) -> Mapping[str, np.ndarray]:
+) -> Mapping[str, np.ndarray]:
     """Apply subhalo mass-modelling (Sultan+ 2020) to all merging halos
 
     This function will create a subhalo dataset containing the modelled masses
@@ -155,7 +182,7 @@ def create_submass_data(
     ----------
     forest
         the full merger tree forest data
-    
+
     simulation
         the simulation string or Simulation instance
 
@@ -171,7 +198,7 @@ def create_submass_data(
 
     compute_fsub_stats
         if True, will calculate fsubtot (total mass of substructure) and
-        fsubmax (mass of the largest substructure), where the numbers are 
+        fsubmax (mass of the largest substructure), where the numbers are
         relative to the host-halo mass. These columns will be added to the
         ``forest`` data.
 
@@ -190,14 +217,14 @@ def create_submass_data(
     cosmo = simulation.cosmo
     a = simulation.step2a(np.array(simulation.cosmotools_steps))
     H = cosmo.hubble_parameter(a)
-    H0 = 100*cosmo.h
+    H0 = 100 * cosmo.h
 
     # Bryan-Norman overdensity
     Delta_vir = cosmo.virial_overdensity(a)
     Delta_vir_0 = cosmo.virial_overdensity(1)
 
     # dynamical time of halo, in Gyr
-    tau_dyn = 1.628 / cosmo.h * (Delta_vir/Delta_vir_0)**-0.5 * (H/H0)**-1
+    tau_dyn = 1.628 / cosmo.h * (Delta_vir / Delta_vir_0) ** -0.5 * (H / H0) ** -1
     # characteristic timescale of subhalo mass loss
     tau_sub = tau_dyn / A
 
@@ -205,43 +232,52 @@ def create_submass_data(
     t_lb = cosmo.lookback_time(a)
 
     # number of (host) halos we're dealing with
-    nhalos = len(forest['snapnum'])
+    nhalos = len(forest["snapnum"])
 
     # apply submass model
     sub_mass, sub_hostidx, sub_infallidx, sub_snapnum = _create_submass_data(
-        forest['tree_node_mass'], 
-        forest['snapnum'], 
-        len(simulation.cosmotools_steps)-1,
-        forest['descendant_idx'],
+        forest["tree_node_mass"],
+        forest["snapnum"],
+        len(simulation.cosmotools_steps) - 1,
+        forest["descendant_idx"],
         tau_sub,
-        t_lb, 
-        zeta
-        )
+        t_lb,
+        zeta,
+    )
 
     subhalo_data = {
-        'mass': sub_mass,
-        'hostidx': sub_hostidx,
-        'infallidx': sub_infallidx,
-        'snapnum': sub_snapnum
+        "mass": sub_mass,
+        "hostidx": sub_hostidx,
+        "infallidx": sub_infallidx,
+        "snapnum": sub_snapnum,
     }
 
     # Apply mass threshold
     if mass_threshold is not None:
-        mask = subhalo_data['mass'] > mass_threshold
+        mask = subhalo_data["mass"] > mass_threshold
         for k in subhalo_data.keys():
             subhalo_data[k] = subhalo_data[k][mask]
 
     # order subdata by host halo
-    subdata_s, subdata_offset, subdata_size = _subdata_hostidx_order(nhalos, subhalo_data['hostidx'])
+    subdata_s, subdata_offset, subdata_size = _subdata_hostidx_order(
+        nhalos, subhalo_data["hostidx"]
+    )
     for k in subhalo_data.keys():
         subhalo_data[k] = subhalo_data[k][subdata_s]
-    forest['subdata_offset'] = subdata_offset
-    forest['subdata_size'] = subdata_size
+    forest["subdata_offset"] = subdata_offset
+    forest["subdata_size"] = subdata_size
 
     # fsub statistics
     if compute_fsub_stats:
-        forest['fsubtot'] = np.zeros(nhalos, dtype=np.float32)
-        forest['fsubmax'] = np.zeros(nhalos, dtype=np.float32)
-        _compute_fsub_stats(forest['subdata_offset'], forest['subdata_size'], forest['tree_node_mass'], subhalo_data['mass'], forest['fsubtot'], forest['fsubmax'])
+        forest["fsubtot"] = np.zeros(nhalos, dtype=np.float32)
+        forest["fsubmax"] = np.zeros(nhalos, dtype=np.float32)
+        _compute_fsub_stats(
+            forest["subdata_offset"],
+            forest["subdata_size"],
+            forest["tree_node_mass"],
+            subhalo_data["mass"],
+            forest["fsubtot"],
+            forest["fsubmax"],
+        )
 
     return subhalo_data
