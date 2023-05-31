b4d3e0e1e5aee3a6012934e2487ab007d987b9b6
Michael Buehlmann <buehlmann.michi@gmail.com>
2023-05-31 09:21:22 -0500

    better core ordering

--- haccytrees/coretrees/assemble.py
+++ haccytrees/coretrees/assemble.py
@@ -1,12 +1,14 @@
-from ..simulations import Simulation
-import pygio
-from mpipartition import Partition, distribute, exchange
-import numpy as np
-from mpi4py import MPI
 from typing import Union
 
-from ..utils.datastores import NumpyStore
 import h5py
+import numba
+import numpy as np
+import pygio
+from mpi4py import MPI
+from mpipartition import Partition, distribute, exchange
+
+from ..simulations import Simulation
+from ..utils.datastores import NumpyStore
 
 tracked_properties = (
     [
@@ -23,9 +25,9 @@ tracked_properties = (
         "infall_fof_halo_center_x",
         "infall_fof_halo_center_y",
         "infall_fof_halo_center_z",
-        "infall_fof_halo_mean_vx",
-        "infall_fof_halo_mean_vy",
-        "infall_fof_halo_mean_vz",
+        "infall_fof_halo_com_vx",
+        "infall_fof_halo_com_vy",
+        "infall_fof_halo_com_vz",
         "infall_step",
         "infall_fof_halo_tag",
         "infall_tree_node_index",
@@ -46,6 +48,90 @@ def _read_data(partition, simulation, coreprops_filename, verbose=True):
         coreprops[x] = np.fmod(coreprops[x] + simulation.rl, simulation.rl)
     coreprops = distribute(partition, simulation.rl, coreprops, "xyz", verbose=verbose)
     return coreprops
+
+
+@numba.jit(nopython=True, parallel=True)
+def _subsort_centrals(coretag, central_start, central_size, host_idx, group_count):
+    ncores = len(coretag)
+    ncentrals = len(central_start)
+    child_count = np.zeros(ncores, dtype=np.int32)
+    my_direct_offset = np.zeros(ncores, dtype=np.int32)
+    my_total_offset = np.zeros(ncores, dtype=np.int32)
+
+    for i in numba.prange(ncentrals):
+        _start = central_start[i]
+        _size = central_size[i]
+        _end = _start + _size
+        # count size of each group
+        for j in range(_start, _end):
+            # self
+            group_count[j] += 1
+            # add to host recursively
+            _idx = j
+            while _idx != _start:
+                _host = host_idx[_idx]
+                group_count[_host] += 1
+                _idx = _host
+
+        # calculate offset of my group among other children of my host
+        for j in range(_start + 1, _end):
+            my_direct_offset[j] = child_count[host_idx[j]]
+            child_count[host_idx[j]] += group_count[j]
+
+        # calculate actual offset
+        my_total_offset[_start] = _start
+        for j in range(_start + 1, _end):
+            my_offset = my_direct_offset[j]
+            _idx = j
+            while _idx != _start:
+                _host = host_idx[_idx]
+                my_offset += my_direct_offset[_host] + 1
+                _idx = _host
+            my_total_offset[j] = my_offset + _start
+
+    return my_total_offset
+
+
+def _sort_core_roots(coretag, hosttag, foftag, central, extensive_asserts=True):
+    # replace hosttag of central with self (by default: 0)
+    hosttag = np.copy(hosttag)
+    hosttag[central > 0] = coretag[central > 0]
+    if extensive_asserts:
+        assert np.all(hosttag > 0)
+        assert np.all(np.isin(hosttag, coretag))
+        assert len(np.unique(foftag)) == np.sum(central)
+
+    # find host index
+    s_core = np.argsort(coretag)
+    coretag_sorted = coretag[s_core]
+    host_idx = np.searchsorted(coretag_sorted, hosttag)
+    host_idx = s_core[host_idx]
+    if extensive_asserts:
+        assert np.all(coretag[host_idx] == hosttag)
+
+    # primary sort: by foftag, with central first
+    s_fof = np.lexsort([-central, foftag])
+    central_start = np.nonzero(central[s_fof])[0]
+    central_size = np.diff(np.append(central_start, len(central)))
+
+    # host_idx for new ordering
+    host_idx = np.argsort(s_fof)[host_idx][s_fof]
+    if extensive_asserts:
+        assert np.all(coretag[s_fof][host_idx] == hosttag[s_fof])
+
+    group_count = np.zeros_like(coretag, dtype=np.int32)
+    s = _subsort_centrals(
+        coretag[s_fof], central_start, central_size, host_idx, group_count
+    )
+    if extensive_asserts:
+        # check we have the correct sizes for the centrals
+        assert np.all(group_count[central_start] == central_size)
+        # check it's a permutation
+        assert np.all(s >= 0)
+        assert np.all(s < len(s))
+        assert len(np.unique(s)) == len(s)
+
+    return s_fof[s], host_idx[s], group_count[s]
 
 
 def reorganize_coreproperties(
@@ -72,12 +158,6 @@ def reorganize_coreproperties(
     )
     tracked_dtypes = {k: d.dtype for k, d in coreprops.items()}
 
-    # order by fof tag and centrals (for each fof tag, centrals come first)
-    # s = np.argsort(coreprops["fof_halo_tag"])
-    s = np.lexsort([-coreprops["central"], coreprops["fof_halo_tag"]])
-    coreprops = {k: d[s] for k, d in coreprops.items()}
-    root_idx = np.nonzero(coreprops["central"])[0]
-
     central_mask = coreprops["central"] == 1
     fof_tags = np.sort(coreprops["fof_halo_tag"][central_mask])
     mask_foreign_cores = np.isin(coreprops["fof_halo_tag"], fof_tags, invert=True)
@@ -94,6 +174,18 @@ def reorganize_coreproperties(
             do_all2all=False,
             replace_notfound_key=None,  # abort if no host found
         )
+
+    # order by fof tag and centrals (for each fof tag, centrals come first)
+    # s = np.argsort(coreprops["fof_halo_tag"])
+    # s = np.lexsort([-coreprops["central"], coreprops["fof_halo_tag"]])
+    s, host_idx, group_count = _sort_core_roots(
+        coreprops["core_tag"],
+        coreprops["host_core"],
+        coreprops["fof_halo_tag"],
+        coreprops["central"],
+    )
+    coreprops = {k: d[s] for k, d in coreprops.items()}
+    root_idx = np.nonzero(coreprops["central"])[0]
 
     owned_core_tags = coreprops["core_tag"]
     owned_core_tags_count = len(owned_core_tags)
@@ -211,4 +303,7 @@ def reorganize_coreproperties(
 
         g_idx = f.create_group("index")
         g_idx.create_dataset("root_idx", data=own_core_offsets[root_idx])
-        # TODO: add index data
+
+        for i, snapnum in enumerate(range(len(steps) - 1, -1, -1)):
+            step = steps[snapnum]
+            npstore.remove(step)
