d90311085adfb59e62a8e3769af910cb9c93c20e
Michael Buehlmann <buehlmann.michi@gmail.com>
2024-05-15 08:37:14 -0500

    update dependencies

--- haccytrees/visualization/mergertree.py
+++ haccytrees/visualization/mergertree.py
@@ -1,41 +1,67 @@
-import numpy as np 
+import numpy as np
 from collections import namedtuple
-import drawSvg
+import drawsvg
 import matplotlib
 from typing import Mapping, Union
 
-class _Halo(namedtuple("_Halo", ['id', 'mass', 'freemassacc', 'scale_fac', 'snapnum', 'depth', 'progenitors'])):
+
+class _Halo(
+    namedtuple(
+        "_Halo",
+        ["id", "mass", "freemassacc", "scale_fac", "snapnum", "depth", "progenitors"],
+    )
+):
     __slots__ = ()
+
     def __str__(self):
         return f"Halo(id={self.id}, mass={self.mass:.2e}, snapnum={self.snapnum}, depth={self.depth}, nprog={len(self.progenitors)})"
+
     def __repr__(self):
         return self.__str__()
 
 
-def _extract_tree(trees, progenitor_array, target_idx, mass_threshold=1e11, max_steps=100):
+def _extract_tree(
+    trees, progenitor_array, target_idx, mass_threshold=1e11, max_steps=100
+):
     hdict = {}
-    snaplists = [[] for d in range(max_steps+1)]
-    hroot = _Halo(target_idx, trees['tree_node_mass'][target_idx], trees['tree_node_mass'][target_idx], trees['scale_factor'][target_idx], trees['snapnum'][target_idx], 0, [])
-    last_step = trees['snapnum'][target_idx]
+    snaplists = [[] for d in range(max_steps + 1)]
+    hroot = _Halo(
+        target_idx,
+        trees["tree_node_mass"][target_idx],
+        trees["tree_node_mass"][target_idx],
+        trees["scale_factor"][target_idx],
+        trees["snapnum"][target_idx],
+        0,
+        [],
+    )
+    last_step = trees["snapnum"][target_idx]
     hdict[target_idx] = hroot
     queue = [hroot]
     snaplists[0].append(hroot)
     while len(queue) > 0:
         h = queue.pop()
-        prog_start = trees['progenitor_offset'][h.id]
-        prog_end = prog_start + trees['progenitor_count'][h.id]
+        prog_start = trees["progenitor_offset"][h.id]
+        prog_end = prog_start + trees["progenitor_count"][h.id]
         progenitors = progenitor_array[prog_start:prog_end]
-        masses = np.array([trees['tree_node_mass'][i] for i in progenitors])
+        masses = np.array([trees["tree_node_mass"][i] for i in progenitors])
         s = np.argsort(masses)[::-1]
         masses = masses[s]
         progenitors = progenitors[s]
-        
+
         next_queue = []
         for m, pidx in zip(masses, progenitors):
             if m < mass_threshold:
                 continue
-            depth = last_step - trees['snapnum'][pidx]
-            p = _Halo(pidx, m, m, trees['scale_factor'][pidx], trees['snapnum'][pidx], depth, [])
+            depth = last_step - trees["snapnum"][pidx]
+            p = _Halo(
+                pidx,
+                m,
+                m,
+                trees["scale_factor"][pidx],
+                trees["snapnum"][pidx],
+                depth,
+                [],
+            )
             hdict[pidx] = p
             h.progenitors.append(p)
             h._replace(freemassacc=h.freemassacc - p.mass)
@@ -47,25 +73,25 @@ def _extract_tree(trees, progenitor_array, target_idx, mass_threshold=1e11, max_
     return hdict, hroot, snaplists
 
 
-def _tree_align(hlist, hloc, snaplists, padding, align='block'):
+def _tree_align(hlist, hloc, snaplists, padding, align="block"):
     hv = np.array([h.mass for h in hlist])
     hy = np.zeros(len(hlist), dtype=np.float64)
     snapmass = np.array([sum(h.mass for h in sl) for sl in snaplists])
-    snaplen  = np.array([len(sl) for sl in snaplists])
-    
-    if align == 'bottom' or align == 'center':
-        snapvtot = snapmass * (1 + (snaplen-1) * padding)
-        snappadding = (snapvtot-snapmass)/np.clip((snaplen-1), 1, None)
-    elif align == 'block':
-        snapvtot = snapmass * (1 + (snaplen-1) * padding)
-        snappadding = (np.max(snapvtot)-snapmass)/np.clip((snaplen-1), 1, None)
+    snaplen = np.array([len(sl) for sl in snaplists])
+
+    if align == "bottom" or align == "center":
+        snapvtot = snapmass * (1 + (snaplen - 1) * padding)
+        snappadding = (snapvtot - snapmass) / np.clip((snaplen - 1), 1, None)
+    elif align == "block":
+        snapvtot = snapmass * (1 + (snaplen - 1) * padding)
+        snappadding = (np.max(snapvtot) - snapmass) / np.clip((snaplen - 1), 1, None)
     else:
         raise NotImplementedError("no align {}".format(align))
 
     # find hy
     for i, sl in enumerate(snaplists):
-        if align == 'center':
-            curoff = 0.5-snapvtot[i]/2
+        if align == "center":
+            curoff = 0.5 - snapvtot[i] / 2
         else:
             curoff = 0
         for j, h in enumerate(sl):
@@ -74,16 +100,20 @@ def _tree_align(hlist, hloc, snaplists, padding, align='block'):
 
     return hv, hy
 
+
 def _tree_recursive(hroot, hlist, hloc, snaplists, padding, align):
-    minpad = 0.1*padding*hroot.mass
-    maxpad = padding*hroot.mass
-    padding_fct = lambda prosizes: np.clip(padding * (prosizes[:-1] + prosizes[1:]), minpad, maxpad)
+    minpad = 0.1 * padding * hroot.mass
+    maxpad = padding * hroot.mass
+    padding_fct = lambda prosizes: np.clip(
+        padding * (prosizes[:-1] + prosizes[1:]), minpad, maxpad
+    )
     # padding_fct = lambda prosizes: padding * (prosizes[:-1] + prosizes[1:])
     # padding_fct = lambda prosizes: padding * (prosizes[:-1] + 5*prosizes[1:])/3
     # padding_fct = lambda prosizes: padding * np.sqrt(prosizes[:-1] * prosizes[1:])
     # Find vertical size of each node
     hv = np.array([h.mass for h in hlist])
     sizes = {}
+
     def get_size(h):
         if len(h.progenitors) == 0:
             size = h.mass
@@ -94,24 +124,28 @@ def _tree_recursive(hroot, hlist, hloc, snaplists, padding, align):
 
         sizes[h.id] = size
         return size
+
     get_size(hroot)
 
     # position nodes (relative to root)
     vpos = {}
-    if align == 'center':
+    if align == "center":
+
         def set_vpos(h, center):
-            vpos[h.id] = center - h.mass/2
+            vpos[h.id] = center - h.mass / 2
             mysize = sizes[h.id]
             prosizes = np.array([sizes[p.id] for p in h.progenitors])
             paddings = padding_fct(prosizes)
             paddings = np.append(paddings, 0)
-            pcenter = center - mysize/2
+            pcenter = center - mysize / 2
             for i, p in enumerate(h.progenitors):
-                pcenter += prosizes[i]/2
+                pcenter += prosizes[i] / 2
                 set_vpos(p, pcenter)
-                pcenter += prosizes[i]/2
+                pcenter += prosizes[i] / 2
                 pcenter += paddings[i]
-    elif align == 'bottom':
+
+    elif align == "bottom":
+
         def set_vpos(h, bottom):
             vpos[h.id] = bottom
             mysize = sizes[h.id]
@@ -123,6 +157,7 @@ def _tree_recursive(hroot, hlist, hloc, snaplists, padding, align):
                 set_vpos(p, pbottom)
                 pbottom += prosizes[i]
                 pbottom += paddings[i]
+
     else:
         raise NotImplementedError(f"unknown align: {align}")
 
@@ -132,19 +167,20 @@ def _tree_recursive(hroot, hlist, hloc, snaplists, padding, align):
 
 
 def merger_tree_drawing(
-        trees: Mapping[str, np.ndarray], 
-        progenitor_array: np.ndarray, 
-        target_idx: int, 
-        *,
-        max_steps: int=20, 
-        mass_threshold: float=1e10, 
-        method: str='block', 
-        padding: float=0.05, 
-        width: int=1200, 
-        height: int=600, 
-        cmap: Union[str, matplotlib.colors.Colormap]='viridis', 
-        coloring: str='branch', 
-        **kwargs) -> drawSvg.Drawing:
+    trees: Mapping[str, np.ndarray],
+    progenitor_array: np.ndarray,
+    target_idx: int,
+    *,
+    max_steps: int = 20,
+    mass_threshold: float = 1e10,
+    method: str = "block",
+    padding: float = 0.05,
+    width: int = 1200,
+    height: int = 600,
+    cmap: Union[str, matplotlib.colors.Colormap] = "viridis",
+    coloring: str = "branch",
+    **kwargs,
+) -> drawsvg.Drawing:
     """Visualize the merger-tree as an svg
 
     Parameters
@@ -190,7 +226,7 @@ def merger_tree_drawing(
 
     Returns
     -------
-    drawing: drawSvg.Drawing
+    drawing: drawsvg.Drawing
         the svg
 
     Notes
@@ -205,17 +241,23 @@ def merger_tree_drawing(
     - bottom
     """
     # Some config
-    soft = kwargs.get('soft', 0.5)
-    nodewidth = kwargs.get('nodewidth', max_steps)
-    mm_threshold = kwargs.get('mm_threshold', 1/3)
-    mm_is_absolute = kwargs.get('mm_is_absolute', False)
-    highlight_mm = kwargs.get('highlight_mm', False)
+    soft = kwargs.get("soft", 0.5)
+    nodewidth = kwargs.get("nodewidth", max_steps)
+    mm_threshold = kwargs.get("mm_threshold", 1 / 3)
+    mm_is_absolute = kwargs.get("mm_is_absolute", False)
+    highlight_mm = kwargs.get("highlight_mm", False)
     if cmap is not None and isinstance(cmap, str):
         cmap = matplotlib.pyplot.get_cmap(cmap)
-    aspect = width/height
-    
+    aspect = width / height
+
     # Get Data
-    hdict, hroot, snaplists = _extract_tree(trees, progenitor_array, target_idx, mass_threshold=mass_threshold, max_steps=max_steps)
+    hdict, hroot, snaplists = _extract_tree(
+        trees,
+        progenitor_array,
+        target_idx,
+        mass_threshold=mass_threshold,
+        max_steps=max_steps,
+    )
 
     # Get a List of all halos
     nhalos = len(hdict)
@@ -224,19 +266,20 @@ def merger_tree_drawing(
     hloc = {h.id: i for i, h in enumerate(hlist)}
 
     # Determine xpos, height and width
-    hnorm = max_steps * (1+nodewidth/100/max_steps)
-    hx = np.array([hroot.snapnum - h.snapnum for h in hlist])/hnorm*aspect
-    
-    
-    hh = np.array([nodewidth/100/max_steps for h in hlist])
+    hnorm = max_steps * (1 + nodewidth / 100 / max_steps)
+    hx = np.array([hroot.snapnum - h.snapnum for h in hlist]) / hnorm * aspect
+
+    hh = np.array([nodewidth / 100 / max_steps for h in hlist])
     po = np.zeros(nhalos, dtype=np.float64)
 
-    methods = method.split('-')
+    methods = method.split("-")
     print(methods, flush=True)
     if len(methods) == 1:
-        methods.append('center')
-    if methods[0] == 'recursive':
-        hv, hy = _tree_recursive(hroot, hlist, hloc, snaplists, padding, align=methods[1])
+        methods.append("center")
+    if methods[0] == "recursive":
+        hv, hy = _tree_recursive(
+            hroot, hlist, hloc, snaplists, padding, align=methods[1]
+        )
     else:
         hv, hy = _tree_align(hlist, hloc, snaplists, padding, align=methods[0])
 
@@ -245,9 +288,9 @@ def merger_tree_drawing(
     voff = np.min(hy)
     vnorm = np.max(hymax) - voff
     # print(voff, vnorm)
-    hy  = (hy-voff)/vnorm
+    hy = (hy - voff) / vnorm
     hv /= vnorm
-            
+
     # get links
     links = [(hloc[h.id], hloc[p.id]) for h in hlist for p in h.progenitors]
     # get progenitor offset for links
@@ -255,61 +298,87 @@ def merger_tree_drawing(
         poff = 0
         for p in h.progenitors:
             po[hloc[p.id]] = poff
-            poff += p.mass/vnorm
+            poff += p.mass / vnorm
 
     if highlight_mm:
         highlight_dict = {h.id: False for h in hlist}
+
         def highlight_branches(h):
             highlight_dict[h.id] = True
             if len(h.progenitors):
                 # highlight main progenitor
                 highlight_branches(h.progenitors[0])
-                abs_threshold = mm_threshold if mm_is_absolute else mm_threshold * h.progenitors[0].mass
+                abs_threshold = (
+                    mm_threshold
+                    if mm_is_absolute
+                    else mm_threshold * h.progenitors[0].mass
+                )
                 for p in h.progenitors[1:]:
                     if p.mass >= abs_threshold:
-                        highlight_branches(p)    
+                        highlight_branches(p)
+
         highlight_branches(hroot)
         highlights = [highlight_dict[h.id] for h in hlist]
-            
+
     else:
         highlights = [True for h in hlist]
 
     # Plot Halos
     if coloring is None:
-        colors = ['#000000' for h in hlist]
-    elif coloring == 'branch':
+        colors = ["#000000" for h in hlist]
+    elif coloring == "branch":
         color_dict = {}
+
         def set_color(h, low, up):
             color_dict[h.id] = cmap(low)
             nprogs = max(1, len(h.progenitors))
             # diff = max(up-low, 0.2)
             up = 1
-            inc = (up-low)/nprogs
+            inc = (up - low) / nprogs
             for i, p in enumerate(h.progenitors):
-                set_color(p, low + i*inc, low + (i+1)*inc)
+                set_color(p, low + i * inc, low + (i + 1) * inc)
+
         set_color(hroot, 0, 1)
         colors = [matplotlib.colors.rgb2hex(color_dict[h.id]) for h in hlist]
     else:
         raise NotImplementedError(f"unknown coloring {coloring}")
 
-
-
-    d = drawSvg.Drawing(aspect, 1, origin=(0,0))
-    d.setRenderSize(aspect*height, height)
-    d.draw(drawSvg.Rectangle(0, 0, aspect, 1, fill='#FFF'))
+    d = drawsvg.Drawing(aspect, 1, origin=(0, 0))
+    d.setRenderSize(aspect * height, height)
+    d.draw(drawsvg.Rectangle(0, 0, aspect, 1, fill="#FFF"))
     for i in range(nhalos):
-        d.append(drawSvg.Rectangle(hx[i], hy[i], hh[i], hv[i], fill=colors[i], fill_opacity=1 if highlights[i] else 0.3))
-        
+        d.append(
+            drawsvg.Rectangle(
+                hx[i],
+                hy[i],
+                hh[i],
+                hv[i],
+                fill=colors[i],
+                fill_opacity=1 if highlights[i] else 0.3,
+            )
+        )
+
     for ld, lp in links:
-        _x0 = hx[ld]+hh[ld]
-        _y0 = hy[ld]+po[lp]
+        _x0 = hx[ld] + hh[ld]
+        _y0 = hy[ld] + po[lp]
         _h = hv[lp]
         _x1 = hx[lp]
         _y1 = hy[lp]
         _dx = _x1 - _x0
         _dy = _y1 - _y0
         _s0 = _dx * soft  # distance to control point 1
-        _s1 = _dx * (1-soft)  # distance to control point 2
-        d.append(drawSvg.Path(stroke='none', fill=colors[lp], fill_opacity=0.6 if highlights[lp] else 0.1).M(_x0, _y0).v(_h).c(_s0, 0, _s1, _dy, _dx, _dy).v(-_h).c(-_s0, 0, -_s1, -_dy, -_dx, -_dy))
+        _s1 = _dx * (1 - soft)  # distance to control point 2
+        d.append(
+            drawsvg.Path(
+                stroke="none",
+                fill=colors[lp],
+                fill_opacity=0.6 if highlights[lp] else 0.1,
+            )
+            .M(_x0, _y0)
+            .v(_h)
+            .c(_s0, 0, _s1, _dy, _dx, _dy)
+            .v(-_h)
+            .c(-_s0, 0, -_s1, -_dy, -_dx, -_dy)
+        )
 
     return d
