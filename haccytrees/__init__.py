55125c97bff69addf293e624dfa61c09364dd88a
Michael Buehlmann <buehlmann.michi@gmail.com>
2024-06-11 10:04:11 -0500

    Bump version: 0.9.1 → 0.10.0

--- haccytrees/__init__.py
+++ haccytrees/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Michael Buehlmann"""
 __email__ = "buehlmann.michi@gmail.com"
-__version__ = "0.9.1"
+__version__ = "0.10.0"
 
 from .simulations import Simulation
 from . import mergertrees
